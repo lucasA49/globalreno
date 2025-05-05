8800ca74e92c07c7d0404a39facca7d92d371af3
Lucasa49 <lucasaksu@gmail.com>
2025-05-05 17:29:08 +0200

    mise en place de la navbaré

--- vite-project/src/Composant/Header.jsx
+++ vite-project/src/Composant/Header.jsx
@@ -1,40 +1,27 @@
-import "../Styles/header.css";
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 
-function Header() {
+export default function Header() {
   return (
-    <>
-      <div className="header">
-        <div>
-          <nav className="nav flex justify-center items-center  ">
-            <NavLink
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold  rounded-full h-9 w-18 items-center "
-              to="/"
-            >
-              Accueil
-            </NavLink>
-            <NavLink
-              className="bg-blue-500 hover:bg-blue-700 text-white font-boldb rounded-full h-9 w-18 items-center"
-              to="/ok"
-            >
-              Toiture
-            </NavLink>
-            <NavLink
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold  rounded-full h-9 w-18 items-center"
-              to="/ok"
-            >
-              Facade
-            </NavLink>
-            <NavLink
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold  rounded-full h-9 w-18 items-center"
-              to="/ok"
-            >
-              Isolation
-            </NavLink>
-          </nav>
-        </div>
-      </div>
-    </>
+    <nav className="w-full flex justify-end items-center bg-gray-300 gap-30 h-15 p-9">
+      {[
+        { to: '/', label: 'Accueil' },
+        { to: '/toiture', label: 'Toiture' },
+        { to: '/facade', label: 'Façade' },
+        { to: '/isolation', label: 'Isolation' },
+        { to: '/devis', label: 'Devis' },
+      ].map(({ to, label }) => (
+        <NavLink
+          key={to}
+          to={to}
+          className={({ isActive }) =>
+            `${
+              isActive ? 'bg-red-500' : 'hover:bg-gray-300'
+            } text-black font-medium rounded-full px-6 py-3 w-32 text-center`
+          }
+        >
+          {label}
+        </NavLink>
+      ))}
+    </nav>
   );
 }
-export default Header;

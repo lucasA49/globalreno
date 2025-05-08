2761891ee03a702be3ed4e23f946582cdf1582b0
lucasA49 <lucasaksu@gmail.com>
2025-05-08 02:34:25 +0200

    mise en forme de la home page

--- vite-project/src/Composant/Carousel.jsx
+++ vite-project/src/Composant/Carousel.jsx
@@ -1,13 +1,13 @@
-
 export default function Carousel() {
- 
-
   return (
-    <div className="w-full bg-white-300 h-[20vh] md:h-[20vh] overflow-hidden relative flex space-between gap-10">
-    <img src="/jefco.webp" alt="" className='h-48'/>
-    <img src="/seigneurie.png" alt="" className="h-48" />
-    <img src="/ultima.png" alt="" className="h-48" />
-    <img src="/gdk.jpg" alt="" className="h-48" />
+    <div>
+      <h4 className="flex mt-2 ml-10 h-7">Nos Partenaires</h4>
+      <div className="w-full bg-white-300 h-[20vh] md:h-[20vh] overflow-hidden relative flex space-between gap-10">
+      <img src="/jefco.webp" alt="" className="h-48" />
+      <img src="/seigneurie.png" alt="" className="h-48" />
+      <img src="/ultima.png" alt="" className="h-48" />
+      <img src="/gdk.jpg" alt="" className="h-48" />
+      </div>
     </div>
   );
 }
--- vite-project/src/Composant/HomeSlider.jsx
+++ vite-project/src/Composant/HomeSlider.jsx
@@ -6,28 +6,28 @@ const panels = [
     title: "Toiture",
     description:
       "Redonnez une seconde jeunesse à vos tuiles et ardoises grâce aux produits exclusifs de Technitoit, le spécialiste de la toiture en France.",
-    image: "/rge.png",
+    image: "rge.png",
     link: "/toiture",
   },
   {
     title: "Façade",
     description:
       "Protégez et embellissez votre façade avec nos solutions durables.",
-    image: "/fabrication-francaise.png",
+    image: "fabrication-francaise.png",
     link: "/facade",
   },
   {
     title: "Isolation",
     description:
       "Améliorez votre confort thermique et réalisez des économies d’énergie.",
-    image: "/img3.jpg",
+    image: "/apres_maison.jpg",
     link: "/isolation",
   },
   {
     title: "Fenêtres",
     description:
       "Optimisez la lumière naturelle et l’isolation de votre maison.",
-    image: "/img4.jpg",
+    image: "ultima.png",
     link: "/fenetres",
   },
 ];
@@ -38,6 +38,7 @@ const ImagePanels = () => {
   return (
     <div className="flex flex-wrap h-auto overflow-hidden">
       {panels.map((panel, index) => (
+       
         <div
           key={index}
           className={`relative w-full sm:w-1/2 md:w-1/4 cursor-pointer transition-all duration-500 ease-in-out transform ${
@@ -49,26 +50,24 @@ const ImagePanels = () => {
           <img
             src={panel.image}
             alt={panel.title}
-            className="w-full h-full object-cover border-2 border-red-500 z-0"
+            className="w-full h-full object-cover border-0.5  z-3"
           />
 
           {/* Overlay */}
           <div
-            className={`absolute inset-0 transition-opacity duration-300 ${
-              index === activeIndex
-                ? "bg-black bg-opacity-40 z-20"
-                : "bg-black bg-opacity-30 z-10"
-            }`}
-          ></div>
+  className={`absolute inset-0 transition-opacity duration-300 
+    ${index === activeIndex ? 'opacity-0' : 'bg-gray-500 opacity-50'}`}
+></div>
+
 
           {/* Contenu actif */}
           {index === activeIndex && (
-            <div className="absolute inset-0 z-30 flex flex-col justify-center items-start p-4 sm:p-6 text-white">
+            <div className="absolute inset-0 z-30 flex flex-col justify-center items-start p-4 sm:p-6 text-red-500">
               <h2 className="text-2xl sm:text-3xl font-bold mb-4">{panel.title}</h2>
               <p className="mb-6 max-w-xs sm:max-w-md">{panel.description}</p>
               <NavLink
                 to={panel.link}
-                className="border border-orange-500 px-4 py-2 text-orange-500 hover:bg-orange-500 hover:text-white transition"
+                className="border   px-4 py-2 text-black-500 hover: hover:text-black transition"
               >
                 découvrir
               </NavLink>
